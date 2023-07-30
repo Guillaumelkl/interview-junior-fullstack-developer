86c2f5a2f58721dada44ce54dbc47da4524c4000
Guillaume <guillaumelkl@gmail.com>
2023-07-30 18:58:33 +0300

    minor changes on the js template file

--- interview-frontend/src/citySearch.js
+++ interview-frontend/src/citySearch.js
@@ -1,33 +1,34 @@
-document.addEventListener('DOMContentLoaded',() => {
-    const form = document.getElementById('citySearchForm');
-    const input = document.getElementById('citySearchInput');
-    const cityResults = document.getElementById('citySearchResults');
+// document.addEventListener('DOMContentLoaded',() => {
+//     const form = document.getElementById('citySearchForm');
+//     const input = document.getElementById('citySearchInput');
+//     const cityResults = document.getElementById('citySearchResults');
+//     const citiesData = "../../cities.json";
+//     console.log(citiesData);
 
+//     form.addEventListener('submit', (e) => {
+//         e.preventDefault();
+//         const search =input.value.trim().toLowerCase();
+//         fetchCities(search)
+//     });
 
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
-        const search =input.value.trim().toLowerCase();
-        getCities(search)
-    });
+//     function fetchCities(search) {
+//         fetch(citiesData)
+//          .then((res) => res.json())
+//          .then((data)=>showCities(data))
+//          .catch((err) => console.log('error to get cities',err));
+//     }
 
-    function getCities(search) {
-        fetch()
-         .then((res) => res.json())
-         .then((data)=>showCities(data))
-         .catch((err) => console.log('error to get cities',err));
-    }
-
-    function showCities(cities) {
-        cityResults.innerHTML = '';
-        if(cities.length == 0) {
-            cityResults.innerHTML = '<li> No city found</li>';
-            return;
-        }
+//     function showCities(cities) {
+//         cityResults.innerHTML = '';
+//         if(cities.length == 0) {
+//             cityResults.innerHTML = '<li> No city found</li>';
+//             return;
+//         }
     
-    cities.forEach((city)=>{
-        const li = document.createElement('li');
-        li.textContent = `${city.name} (Count: &{city.count})`;
-        cityResults.appendChild(li)
-    })
-  }
-})
+//     cities.forEach((city)=>{
+//         const li = document.createElement('li');
+//         li.textContent = `${city.name} (Count: ${city.count})`;
+//         cityResults.appendChild(li)
+//     })
+//   }
+// })
